2ae10ca79da61a0946acdc18c4fc01c102841385
NoirhommeSamuel <etu59591@henallux.be>
2026-04-21 11:37:18 +0000

    ajout du fichier python + import

---
+++ prog.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import numpy as np
+import matplotlib.pyplot as plt
+import fidle.pwk as pwk
