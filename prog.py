f32262134fc8051b326c278cf0c298525c25b661
NoirhommeSamuel <etu59591@henallux.be>
2026-04-21 14:51:35 +0000

    delete prog useless

--- prog.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-
-import numpy as np
-import matplotlib.pyplot as plt
-import fidle.pwk as pwk
